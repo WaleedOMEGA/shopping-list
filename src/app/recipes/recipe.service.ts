8f47d9200e8bccceea529fce810a1844923b653b
WaleedOMEGA <waleedomega@hotmail.com>
2019-02-25 10:37:24 +0200

    routing final

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -31,5 +31,8 @@ export class RecipeService {
   addIngredientToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
   }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 }
 
